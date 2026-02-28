3a4c07f003a52f93c0369ff28ea0e0a9c4a08b0f
A2D-mohd-mizna-ansari <mohd.ansari@a2deats.com>
2026-03-01 02:07:16 +0530

    pool

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -9,7 +9,7 @@
     user: process.env.DATABASE_USER,
     password: process.env.DATABASE_PASSWORD,  
     database: process.env.DATABASE_NAME,
-    connectionLimit: 30, // adjust to your MySQL limit
+    connectionLimit: 1, // adjust to your MySQL limit
   })
 
   // Singleton pattern so the pool is reused
