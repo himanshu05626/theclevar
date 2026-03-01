bdc32eae94eb24d1b73d95c2c2d1e9e906111387
A2D-mohd-mizna-ansari <mohd.ansari@a2deats.com>
2026-03-02 01:55:03 +0530

    fix

--- app/(customer)/(routing)/product/[...slug]/ViewProductDetail.jsx
+++ app/(customer)/(routing)/product/[...slug]/ViewProductDetail.jsx
@@ -181,9 +181,7 @@ export default function ProductDetailClient({
           <div className="space-y-6 md:space-y-8">
 
             <div>
-              <p className="text-xs sm:text-sm text-gray-400">
-                SKU: {product.sku}
-              </p>
+            
 
               <h1 className="text-xl sm:text-2xl md:text-3xl lg:text-4xl font-bold mt-2">
                 {product.name}
