e3ef5b4f7a0e2e983f4e259525d6762edacd19be
A2D-mohd-mizna-ansari <mohd.ansari@a2deats.com>
2026-03-25 19:50:37 +0530

    d

--- app/(customer)/api/auth/login/route.js
+++ app/(customer)/api/auth/login/route.js
@@ -109,7 +109,7 @@ export async function POST(req) {
     response.cookies.set("authToken", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "strict",
+      sameSite: "lax",
       path: "/",
       maxAge: 60 * 60 * 24 * 30,
     });
